ce6144b0d12e6398049add7cfff735e5ed87a212
Matt Holsen <matt@mattholsen.com>
2014-11-07 14:07:02 -0500

    initial

---
+++ Vagrantfile
@@ -0,0 +1,27 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+Vagrant.configure("2") do |config|
+    config.vm.box = "ubuntu/trusty64"
+    config.vm.box_url = "https://vagrantcloud.com/ubuntu/trusty64"
+    
+    config.vm.network :private_network, ip: "192.168.33.10"
+    config.vm.network "forwarded_port", guest: 80, host: 8765
+
+
+    config.vm.provider :virtualbox do |v|
+        v.customize ["modifyvm", :id, "--natdnshostresolver1", "on"]
+        v.customize ["modifyvm", :id, "--memory", 1024]
+        v.customize ["modifyvm", :id, "--name", "D8Lab"]
+    end
+
+    config.vm.synced_folder "/home/matt/vagd7/www", "/var/www", create: true,type: "nfs"
+    #,:mount_options => ["dmode=777","fmode=666"]
+    config.vm.synced_folder "/home/matt/vagd7/sqldump", "/var/sqldump", create: true
+    config.vm.synced_folder "/home/matt/vagd7/scripts", "/var/scripts", create: true
+    config.vm.synced_folder "/home/matt/vagd7/custom_config_files", "/var/custom_config_files", create: true
+
+    config.vm.provision :shell, :path => "bootstrap.sh"
+    
+    # config.vm.provision :shell, run: "always", :path => "load.sh"
+end
---
+++ bootstrap.sh
@@ -0,0 +1,71 @@
+# Configure MySQL root password
+debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password password wwsf2wstwv'
+debconf-set-selections <<< 'mysql-server-5.5 mysql-server/root_password_again password wwsf2wstwv'
+
+# Install packages
+apt-get update
+apt-get -y install mysql-server-5.5 php5-mysql libsqlite3-dev apache2 php5 php5-dev php5-gd build-essential php-pear ruby1.9.1-dev
+
+# Set timezone
+echo "America/New_York" | tee /etc/timezone
+dpkg-reconfigure --frontend noninteractive tzdata
+
+# Setup database
+echo "DROP DATABASE IF EXISTS test" | mysql -uroot -pwwsf2wstwv
+echo "CREATE USER 'drupal7'@'localhost' IDENTIFIED BY 'wwsf2wstwv'" | mysql -uroot -pwwsf2wstwv
+echo "CREATE DATABASE drupal7" | mysql -uroot -pwwsf2wstwv
+echo "GRANT ALL ON drupal7.* TO 'drupal7'@'localhost'" | mysql -uroot -pwwsf2wstwv
+echo "FLUSH PRIVILEGES" | mysql -uroot -pwwsf2wstwv
+
+# Apache changes
+# add line to end of conf file
+echo "<Directory /var/www/drupal7>" >> /etc/apache2/apache2.conf
+echo "      Options Indexes FollowSymLinks" >> /etc/apache2/apache2.conf
+echo "      AllowOverride All" >> /etc/apache2/apache2.conf
+echo "      Require all granted" >> /etc/apache2/apache2.conf
+echo " </Directory>" >> /etc/apache2/apache2.conf
+echo "ServerName localhost" >> /etc/apache2/apache2.conf
+echo "User vagrant" >> /etc/apache2/apache2.conf
+echo "Group vagrant" >> /etc/apache2/apache2.conf
+a2enmod rewrite
+# rewrite default.conf from provided source (nfs-linked to host)
+cat /var/custom_config_files/apache2/default | tee /etc/apache2/sites-available/000-default.conf
+
+# Install Mailcatcher
+echo "Installing mailcatcher"
+gem install mailcatcher --no-ri --no-rdoc
+mailcatcher --http-ip=192.168.33.20
+
+# Configure PHP
+sed -i '/;sendmail_path =/c sendmail_path = "/usr/local/bin/catchmail"' /etc/php5/apache2/php.ini
+sed -i '/display_errors = Off/c display_errors = On' /etc/php5/apache2/php.ini
+sed -i '/error_reporting = E_ALL & ~E_DEPRECATED/c error_reporting = E_ALL | E_STRICT' /etc/php5/apache2/php.ini
+sed -i '/html_errors = Off/c html_errors = On' /etc/php5/apache2/php.ini
+
+# Make sure things are up and running as they should be (generates warning if port in use...)
+service apache2 restart
+
+# install drush (just apt-get for the version we need to install Drupal 7)
+apt-get -y install drush
+
+# echo "downloading drupal 7 with drush.  Untarring takes a long time.  Go for a walk..."
+cd /var/www/
+drush dl drupal-7
+
+### drush dl leaves an unpacked dir which I want to rename
+mv dru* drupal7
+
+# add porgie and tirebiter (if you gotta ask you don't need to know....)
+ cp /var/scripts/porgie.php /var/www/drupal7/porgie.php
+ cp /var/scripts/tirebiter.php  /var/www/drupal7/tirebiter.php
+
+ mkdir     /var/www/drupal7/sites/default/files
+ chmod 777 /var/www/drupal7/sites/default/files
+
+cp /var/www/drupal7/sites/default/default.settings.php /var/www/drupal7/sites/default/settings.php
+chmod 777 /var/www/drupal7/sites/default/settings.php
+
+
+# install drupal 7 w drush, with db creds above
+cd /var/www/drupal7
+drush -y site-install standard --db-url='mysql://drupal7:wwsf2wstwv@localhost/drupal7' --site-name="drupal7 Lab"  --account-name=admin --account-pass=bigsam96
