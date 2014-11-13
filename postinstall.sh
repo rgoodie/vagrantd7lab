df8ce0634563521a1c07016e8113e095bdddda07
Richard <r.chardgoodrow@gmail.com>
2014-11-13 15:56:35 -0600

    More stuff

--- postinstall.sh
+++ postinstall.sh
@@ -1,3 +1,9 @@
 cd /var/www/drupal7
 drush dis overlay -y
-drush en og coffee module_filter devel -y
+drush en og -y
+drush en og_access -y
+drush en node_export -y
+drush en features ftools -y
+drush en coffee -y
+drush en module_filter -y
+drush en devel -y
