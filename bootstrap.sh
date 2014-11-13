2de96915b71627c9249132c0f137878b5907ae87
Richard <r.chardgoodrow@gmail.com>
2014-11-13 16:43:23 -0600

    Revert "Going low-security"
    This reverts commit 92979c53d862fbbb1887952cf7192feec6f04eaa.

--- bootstrap.sh
+++ bootstrap.sh
@@ -68,7 +68,7 @@ chmod 777 /var/www/drupal7/sites/default/settings.php
 
 # install drupal 7 w drush, with db creds above
 cd /var/www/drupal7
-drush -y site-install standard --db-url='mysql://root:root@localhost/drupal7' --site-name="drupal7 Lab"  --account-name=admin --account-pass=admin
+drush -y site-install standard --db-url='mysql://drupal7:wwsf2wstwv@localhost/drupal7' --site-name="drupal7 Lab"  --account-name=admin --account-pass=admin
 
 # coffee
 drush dl coffee module_filter devel -y
