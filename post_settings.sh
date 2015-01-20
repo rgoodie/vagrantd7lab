c2c00a747d57ffd3ea277adbe694a37c429ff5a9
RG <r.chardgoodrow@gmail.com>
2015-01-20 11:22:36 -0600

    Module builder

--- post_settings.sh
+++ post_settings.sh
@@ -2,8 +2,8 @@
 cd /var/www/drupal7
 
 # we want module_filter, coffee, and devel
-drush dl -y coffee module_filter devel
-drush en -y coffee module_filter devel
+drush dl -y coffee module_filter devel module_builder
+drush en -y coffee module_filter devel module_builder
 
 # kill overlay
 drush dis overlay -y
